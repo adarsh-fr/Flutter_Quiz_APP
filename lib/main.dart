dd6ac5ebbc25b16371d74b86db6c0baf31c77257
adarsh-fr <adarsh.fr@gmail.com>
2023-04-21 13:05:04 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      )
+    )
+  );
+}
